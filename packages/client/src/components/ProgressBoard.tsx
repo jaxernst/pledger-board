37a5c623b2e7aaca892a942c47881c2819a0b489
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-05-23 14:50:55 -0700

    Add spinner for loading

--- packages/client/src/components/ProgressBoard.tsx
+++ packages/client/src/components/ProgressBoard.tsx
@@ -3,6 +3,7 @@ import { useMUD } from "../MUDContext";
 import { useEntityQuery, useObservableValue } from "@latticexyz/react";
 import { CommitmentStatus } from "../types";
 import { CommitmentCard } from "./CommitmentCard";
+import { RingLoader } from "react-spinners";
 
 const ColBody = "bg-indigo-100 overflow-auto";
 const CardsContainer =
@@ -54,56 +55,71 @@ export const ProgressBoard = ({ klass }: { klass: string }) => {
     return blockTime > deadline;
   });
 
+  const loading = [
+    failedCommitments,
+    finalizedCommitments,
+    ratingZoneCommitments,
+    attestionZoneCommitments,
+  ].every((x) => x.length === 0);
+
   return (
-    <div className={`flex flex-grow ${klass}`}>
-      <div className={ZoneContainerCol + " flex-grow"}>
-        <div className=" bg-blue-400  px-2 font-bold text-zinc-700">
-          Rating Zone
+    <>
+      <div className={`flex flex-grow ${klass}`}>
+        <div className={ZoneContainerCol + " scroll flex-grow snap-center"}>
+          <div className=" bg-blue-400  px-2 font-bold text-zinc-700">
+            Rating Zone
+          </div>
+          <div className={ColBody}>
+            <div className={CardsContainer}>
+              {ratingZoneCommitments.map((id) => (
+                <CommitmentCard key={id} id={id} zone="rating" />
+              ))}
+            </div>
+          </div>
         </div>
-        <div className={ColBody}>
-          <div className={CardsContainer}>
-            {ratingZoneCommitments.map((id) => (
-              <CommitmentCard key={id} id={id} zone="rating" />
-            ))}
+        <div className={ZoneContainerCol}>
+          <div className="bg-violet-400  px-2 font-bold text-zinc-700">
+            Attestation Zone
+          </div>
+          <div className={ColBody}>
+            <div className={CardsContainer}>
+              {attestionZoneCommitments.map((id) => (
+                <CommitmentCard key={id} id={id} zone="attesting" />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={ZoneContainerCol}>
+          <div className=" bg-green-500   px-2 font-bold text-zinc-700">
+            Completion Zone
+          </div>
+          <div className={ColBody}>
+            <div className={CardsContainer}>
+              {finalizedCommitments.map((id) => (
+                <CommitmentCard key={id} id={id} zone="finalized" />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={ZoneContainerCol}>
+          <div className=" bg-orange-400 px-2 font-bold text-zinc-700">
+            Missed Deadline Zone
+          </div>
+          <div className={ColBody}>
+            <div className={CardsContainer}>
+              {failedCommitments.map((id) => (
+                <CommitmentCard key={id} id={id} zone="failed" />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-      <div className={ZoneContainerCol}>
-        <div className="bg-violet-400  px-2 font-bold text-zinc-700">
-          Attestation Zone
+      {loading ? (
+        <div className="fixed left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-4">
+          <RingLoader color="#8044ca" />
+          <i className=" text-xs font-bold">{"Syncing blockchain data..."}</i>
         </div>
-        <div className={ColBody}>
-          <div className={CardsContainer}>
-            {attestionZoneCommitments.map((id) => (
-              <CommitmentCard key={id} id={id} zone="attesting" />
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className={ZoneContainerCol}>
-        <div className=" bg-green-500   px-2 font-bold text-zinc-700">
-          Completion Zone
-        </div>
-        <div className={ColBody}>
-          <div className={CardsContainer}>
-            {finalizedCommitments.map((id) => (
-              <CommitmentCard key={id} id={id} zone="finalized" />
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className={ZoneContainerCol}>
-        <div className=" bg-orange-400 px-2 font-bold text-zinc-700">
-          Missed Deadline Zone
-        </div>
-        <div className={ColBody}>
-          <div className={CardsContainer}>
-            {failedCommitments.map((id) => (
-              <CommitmentCard key={id} id={id} zone="failed" />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 };
