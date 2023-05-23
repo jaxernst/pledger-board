7c37a202158ab222d3955109c3a4ff4b0493b653
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-05-23 13:26:15 -0700

    Rebuild

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -6,6 +6,7 @@ import { MUDProvider } from "./MUDContext";
 
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
+const useDevTools = !import.meta.env.VITE_PROD;
 const root = ReactDOM.createRoot(rootElement);
 
 // TODO: figure out if we actually want this to be async or if we should render something else in the meantime
@@ -15,5 +16,5 @@ setup().then((result) => {
       <App />
     </MUDProvider>
   );
-  // mountDevTools();
+  useDevTools && mountDevTools();
 });
